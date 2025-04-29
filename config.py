b089ad6c47d19ecc906326774fe7e3b972ce50f8
KrzysztofPiekarski <krzysztof.piekarski24@gmail.com>
2025-04-29 13:23:38 +0200

    changes in config

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ load_dotenv()  # Załaduj dane z pliku .env
 
 class Config:
     # DigitalOcean Spaces
-    AWS_REGION = os.environ.get("AWS_REGION", "eu-central-1")
-    AWS_ENDPOINT_URL_S3 = os.environ.get("AWS_ENDPOINT_URL_S3", "")
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+    AWS_REGION = os.environ.get("AWS_REGION")
+    AWS_ENDPOINT_URL_S3 = os.environ.get("AWS_ENDPOINT_URL_S3")
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     
     # Inne ustawienia, np. OpenAI
-    OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
-    LANGFUSE_PUBLIC_KEY = os.environ.get("LANGFUSE_PUBLIC_KEY", "")
-    LANGFUSE_SECRET_KEY = os.environ.get("LANGFUSE_SECRET_KEY", "")
-    LANGFUSE_HOST = os.environ.get("LANGFUSE_HOST", "")
+    OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+    LANGFUSE_PUBLIC_KEY = os.environ.get("LANGFUSE_PUBLIC_KEY")
+    LANGFUSE_SECRET_KEY = os.environ.get("LANGFUSE_SECRET_KEY")
+    LANGFUSE_HOST = os.environ.get("LANGFUSE_HOST")
 
     # Model S3
     BUCKET_NAME = "halfmarathon"
